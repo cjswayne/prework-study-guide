b125f82283118b73a925a733050a0464ae3db21a
cjswayne <134882908+cjswayne@users.noreply.github.com>
2023-12-01 20:17:40 -0800

    adding js logic

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,7 +1,25 @@
-var topics = "HTML, CSS, Git, JavaScript";
+function listTopics(topics){
+    console.log('Here are the topics we learned through prework')
+    let topicsArray = topics
+    topicsArray.forEach(element => {
+        console.log(element);
+    });
+}
 
-var topicsArray = topics.split(", ")
+function selectTopic(topics){
+    console.log("Which topic should we study first")
+    let topicsArray = topics;
+    let randomIndex = Math.floor(Math.random() * topicsArray.length);
+    let selectedTopic = topicsArray[randomIndex];
+    console.log(`Lets study ${selectedTopic}!`);
+}
 
-topicsArray.forEach(element => {
-    console.log(element);
-});
+
+
+
+
+var elements = document.querySelectorAll(".card h2");
+var topicsArray = Array.from(elements).map(element => element.textContent);
+listTopics(topicsArray);
+selectTopic(topicsArray);
+console.log(topicsArray);
